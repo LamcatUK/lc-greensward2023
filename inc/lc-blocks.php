cbfac0fb8df62ace4d80e1169f6e43bff50dcfe8
CheechRockwizard <github@lamcat.co.uk>
2023-08-05 11:36:02 +0100

    wide cta

--- inc/lc-blocks.php
+++ inc/lc-blocks.php
@@ -120,6 +120,15 @@ function acf_blocks()
             'supports' => array('mode' => false),
         ));
         acf_register_block_type(array(
+            'name'				=> 'lc_wide_cta',
+            'title'				=> __('LC Wide CTA'),
+            'category'			=> 'layout',
+            'icon'				=> 'cover-image',
+            'render_template'	=> 'page-templates/blocks/lc_wide_cta.php',
+            'mode'	=> 'edit',
+            'supports' => array('mode' => false),
+        ));
+        acf_register_block_type(array(
             'name'				=> 'lc_full_width_text',
             'title'				=> __('LC Full Width Text'),
             'category'			=> 'layout',
---
+++ page-templates/blocks/lc_wide_cta.php
@@ -0,0 +1,12 @@
+<section class="wide_cta py-5">
+    <div class="container-xl">
+        <div class="wide_cta__card bg--grad">
+            <div class="wide_cta__content">
+                <h3>Contact GreenSward Today</h3>
+                <p>Get in touch with us today with any questions or <a href="/book-survey/">book a no-obligation site
+                        survey</a>.</p>
+                <a href="/contact-us/" class="btn btn-highlight">Contact Us</a>
+            </div>
+        </div>
+    </div>
+</section>
