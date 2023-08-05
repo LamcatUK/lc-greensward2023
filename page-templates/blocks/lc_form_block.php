c696e3c631b4fee9c15e62a13ed6e1e6cdcd9fe6
CheechRockwizard <github@lamcat.co.uk>
2023-08-05 14:33:40 +0100

    contact form stuff

--- page-templates/blocks/lc_form_block.php
+++ page-templates/blocks/lc_form_block.php
@@ -7,15 +7,7 @@ $bg = get_field('theme') == 'Dark' ? 'bg--grey' : '';
 <section class="form_block py-5 <?=$bg?>">
     <div class="container-xl">
         <h2><?=$title?></h2>
-        <div class="row">
-            <div class="col-md-6">
-                <div class="intro mb-4">
-                    <?=$content?>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <?=do_shortcode('[contact-form-7 id="' . $formid . '"]')?>
-            </div>
-        </div>
+        <div class="mb-4"><?=$content?></div>
+        <?=do_shortcode('[contact-form-7 id="' . $formid . '"]')?>
     </div>
 </section>
