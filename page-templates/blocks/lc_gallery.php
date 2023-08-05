75bc770e02840f2eac150079e5ea3c5170f26a4c
CheechRockwizard <github@lamcat.co.uk>
2023-08-05 13:52:50 +0100

    Update lc_gallery.php

--- page-templates/blocks/lc_gallery.php
+++ page-templates/blocks/lc_gallery.php
@@ -13,3 +13,14 @@ foreach (get_field('gallery') as $img) {
         </div>
     </div>
 </section>
+<?php
+add_action('wp_footer', function () {
+    ?>
+<script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.umd.js"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@5.0/dist/fancybox/fancybox.css" />
+<script>
+    Fancybox.bind("[data-fancybox]", {});
+</script>
+<?php
+}, 9999);
+            ?>
