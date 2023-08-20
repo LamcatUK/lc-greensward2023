2f0db608cd11b05cab0dd833ea41e54f85a85d70
CheechRockwizard <github@lamcat.co.uk>
2023-08-20 15:28:07 +0100

    team cards

---
+++ page-templates/blocks/lc_team.php
@@ -0,0 +1,39 @@
+<section class="team">
+    <div class="container-xl py-5">
+        <h2><?=get_field('title')?></h2>
+        <div class="row g-4 justify-content-center">
+            <?php
+while(have_rows('team')) {
+    the_row();
+    ?>
+            <div class="col-lg-6">
+                <img class="team__img"
+                    src="<?=wp_get_attachment_image_url(get_sub_field('image'), 'full')?>">
+                <div class="team__name">
+                    <?=get_sub_field('name')?>
+                </div>
+                <div class="team__role">
+                    <?=get_sub_field('role')?>
+                </div>
+                <div class="team__bio">
+                    <?=get_sub_field('bio')?>
+                </div>
+                <?php
+                if (get_sub_field('email') ?? null) {
+                    ?>
+                <div class="team__bio">
+                    <a
+                        href="mailto:<?=get_sub_field('email')?>"><i
+                            class="fa-solid fa-envelope"></i>
+                        <?=get_sub_field('email')?></a>
+                </div>
+                <?php
+                }
+    ?>
+            </div>
+            <?php
+}
+        ?>
+        </div>
+    </div>
+</section>
