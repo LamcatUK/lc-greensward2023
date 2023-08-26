c3f38f102da1e20179195a0e7343ec420f91d8b8
CheechRockwizard <github@lamcat.co.uk>
2023-08-26 14:01:00 +0100

    title Case

--- page-templates/blocks/lc_work_cta.php
+++ page-templates/blocks/lc_work_cta.php
@@ -1,6 +1,6 @@
 <section class="work_cta">
     <div class="container-xl d-flex flex-wrap justify-content-between align-items-center">
-        <h2 class="h3 mb-0 text-white">Showcasing our workmanship</h2>
+        <h2 class="h3 mb-0 text-white">Showcasing Our Workmanship</h2>
         <a href="/our-work/" class="btn btn-highlight">View our work</a>
     </div>
 </section>
