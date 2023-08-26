05e24518d0ee51afbfa79daf719d2e0d49c7720f
CheechRockwizard <github@lamcat.co.uk>
2023-08-26 15:51:37 +0100

    clients grid

--- page-templates/blocks/lc_clients_grid.php
+++ page-templates/blocks/lc_clients_grid.php
@@ -1,13 +1,14 @@
 <section class="clients py-5">
     <div class="container-xl">
         <h2 class="h3">Some of Our Esteemed Clients</h2>
-        <div class="clients__grid">
+        <div class="clients__grid mt-4">
             <?php
         while (have_rows('clients', 'options')) {
             the_row();
             ?>
             <a href="<?=get_sub_field('site')?>"
-                target="_blank" rel="nofollow noopener" class="clients__card">
+                target="_blank" rel="nofollow noopener" class="clients__card"
+                title="<?=get_sub_field('name')?>">
                 <div class="clients__client">
                     <img src="<?=wp_get_attachment_image_url(get_sub_field('logo'), 'large')?>"
                         alt="<?=get_sub_field('name')?>">
