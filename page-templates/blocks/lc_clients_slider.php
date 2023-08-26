6cb17458115fe4864f2f4c03fc7cbe2252fcb26a
CheechRockwizard <github@lamcat.co.uk>
2023-08-26 14:00:12 +0100

    Update lc_clients_slider.php

--- page-templates/blocks/lc_clients_slider.php
+++ page-templates/blocks/lc_clients_slider.php
@@ -1,6 +1,6 @@
 <section class="clients py-5">
     <div class="container-xl">
-        <h2>Some of our esteemed clients</h2>
+        <h2 class="h3">Some of our esteemed clients</h2>
         <div class="clients__slider">
             <?php
         while (have_rows('clients', 'options')) {
